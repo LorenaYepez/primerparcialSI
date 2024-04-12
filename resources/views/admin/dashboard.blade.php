420f81b6fcc37b45384ab59bef9b2a8767e3d185
Lo Yépez <lorenaponce246@gmail.com>
2024-04-12 18:31:47 -0400

    update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <section class="section">
     <div class="section-header">
-      <h1>Dashboard111</h1>
+      <h1>Dashboard</h1>
     </div>
     <div class="row">
       <div class="col-lg-3 col-md-6 col-sm-6 col-12">
