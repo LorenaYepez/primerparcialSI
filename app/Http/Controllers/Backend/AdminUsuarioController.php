eb33fa0d607b35e06f542a41a7b8d8e5b305080d
@leonardoayala <33144217+laionerleo@users.noreply.github.com>
2024-04-22 22:12:05 -0400

    cmbios paa api resta

--- app/Http/Controllers/Backend/AdminUsuarioController.php
+++ app/Http/Controllers/Backend/AdminUsuarioController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminUsuarioController extends Controller
 {
@@ -31,6 +32,29 @@ public function crear(Request $request)
    // Mostrar un mensaje de éxito o redirigir a otra página
    return redirect()->route("admin.usuario")->with("success", "Tarea creada correctamente");
 }
+
+
+
+public function loginapi(Request $request)
+{
+    $credentials = $request->only('email', 'password');
+
+    if (Auth::attempt($credentials)) {
+        $user = Auth::user();
+        $token = $user->createToken('auth_token');
+
+        return response()->json([
+            'message' => 'Successfully logged in!',
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ], 200);
+    } else {
+        return response()->json([
+            'message' => 'Invalid credentials.'
+        ], 401);
+    }
+}
+
 
 
 }
